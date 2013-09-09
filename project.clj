911a33a740a96954b8dcfb4eee2e515d7a5633c1
Sam Aaron <samaaron@gmail.com>
2013-09-09 10:59:15 +0100

    try dommy

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject defining-pi "0.0.0-SNAPSHOT"
   :description "Defining Pi ClojureScript source"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [prismatic/dommy "0.1.1"]]
   :plugins [[lein-cljsbuild "0.3.2"]]
   :cljsbuild {
               :builds [{:source-paths ["src-cljs"]
