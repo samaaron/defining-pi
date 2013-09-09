29e44623b4af542e0ed461e40ec8841064912734
Sam Aaron <samaaron@gmail.com>
2013-09-09 10:58:07 +0100

    add dom until fns (this stuff should all end up being replaced by a decent external lib soon)

--- src-cljs/defpi/dom.cljs
+++ src-cljs/defpi/dom.cljs
@@ -15,5 +15,15 @@
   [el s]
   (set! (.-innerHTML el) s))
 
+(defn append-child!
+  "Append the dom element c to element el as a child."
+  [el c]
+  (.appendChild el c))
+
+(defn insert-before!
+  "Incert the dom element c to before element el"
+  [el c]
+  (.insertBefore el c))
+
 (defn get-html [el]
   (.-innerHTML el))
