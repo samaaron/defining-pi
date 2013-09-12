327e6203c26fe542c7bb498c50d7617361ee3852
Sam Aaron <samaaron@gmail.com>
2013-09-12 08:01:29 +0100

    update to latest clojurescript - for faster compile times!

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject defining-pi "0.0.0-SNAPSHOT"
   :description "Defining Pi ClojureScript source"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [prismatic/dommy "0.1.1"]]
+                 [prismatic/dommy "0.1.1"]
+                 [org.clojure/clojurescript "0.0-1877"]]
   :plugins [[lein-cljsbuild "0.3.2"]]
   :cljsbuild {
               :builds [{:source-paths ["src-cljs"]
