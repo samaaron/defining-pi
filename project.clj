3a87196be56f2007a54a1721ea71a4cef4cca6d6
Sam Aaron <samaaron@gmail.com>
2013-09-26 18:59:53 -0400

    Add clojurescript repl tools

--- project.clj
+++ project.clj
@@ -2,8 +2,11 @@
   :description "Defining Pi ClojureScript source"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [prismatic/dommy "0.1.1"]
-                 [org.clojure/clojurescript "0.0-1877"]]
+                 [org.clojure/clojurescript "0.0-1896"]
+                 [overtone "0.9.0-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "0.3.2"]]
+  :source-paths ["src-cljs"]
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.1"]]}}
   :cljsbuild {
               :builds [{:source-paths ["src-cljs"]
                         :compiler {:output-to "public/js/cljs-main.js"
